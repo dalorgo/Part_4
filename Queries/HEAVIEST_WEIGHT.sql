23aedf5b7ba75c0eddd640394bbaf41113a4a359
rileypruitt <rileypruitt@me.com>
2014-04-07 15:28:51 -0500

    Updated heaviest pokemon, insert and query log
    Query log almost done, just needs daisy's query
    Updated insert and heaviest weight queries

--- Queries/HEAVIEST_WEIGHT.sql
+++ Queries/HEAVIEST_WEIGHT.sql
@@ -1,5 +1,5 @@
 /* select MAX(cast(substring(Height, 1,locate('lbs', Height)-1) as DECIMAL(10,1))) from Pokemon; */
-
+/*
 SELECT
 	p.Name,
 	p.Height Weight
@@ -9,3 +9,13 @@ WHERE
 	CAST(SUBSTRING(p.Height, 1, LOCATE('lbs', p.Height)-1) AS DECIMAL(10,1)) >= ALL 
 		(SELECT CAST(SUBSTRING(Height, 1, LOCATE('lbs', Height)-1) AS DECIMAL(10,1)) FROM Pokemon)
 	AND p.Height IS NOT NULL;
+*/
+
+SELECT
+	p.Name,
+	p.Height Weight
+FROM
+	Pokemon p
+WHERE
+	(p.NationalID <= 151 AND p.Weight >= ALL (SELECT x.Weight FROM Pokemon x WHERE x.NationalID <= 151)) OR
+	(p.NationalID > 151 AND p.Weight >= ALL (SELECT y.Weight FROM Pokemon y WHERE y.NationalID > 151));
